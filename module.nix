c6efa3ebb8a6abcfd4e42ced7b31a5ac60efd36d
chessai <chessai1996@gmail.com>
2019-07-24 11:52:32 -0400

    add nixos module that people can import

---
+++ module.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+config = {
+  nixpkgs.overlays = [
+    (super: self: {
+      pong = self.callPackage ./default.nix {};
+    })
+  ];
+
+  security.wrappers.pong = {
+    source = "${pkgs.pong}/bin/pong";
+    capabilities = "cap_net_raw+ep";
+  };
+
+  environment.systemPackages = [ pkgs.pong ];
+}
